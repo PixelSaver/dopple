f2b422a2ab4ebd2104ee9606e65f44908d92dd09
PixelSaver <stephen.k@proton.me>
2026-07-07 12:54:44 -0700

    Responds for user

--- index.ts
+++ index.ts
@@ -6,6 +6,16 @@ const app = new App({
     socketMode: true,
 });
 
+app.message(async (event) => {
+    if (event.payload.subtype) return;
+    if (event.payload.user !== process.env.ID) {
+    };
 
+    // await app.client.chat.postMessage({
+    //     channel: event.payload.channel,
+    //     text: ':miau2:',
+    // });
+    event.say(':miau2: <@' + process.env.ID + '> :miau:');
+})
 
 await app.start();
